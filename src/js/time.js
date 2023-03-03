0d10684884f22417f1b0e29eaa2ad6a4ba0be07b
EtoZheLamia <Andariel2175@yandex.ru>
2023-03-03 14:57:43 +0300

    feat: added time and date display

---
+++ src/js/time.js
@@ -0,0 +1,19 @@
+const TIME = document.querySelector('.time');
+const DATE = document.querySelector('.date');
+const dateOptions = {weekday: 'long', month: 'long', day: 'numeric', timeZone: 'UTC'};
+
+function showDate() {
+  const date = new Date();
+  const currentDate = date.toLocaleDateString('ru-RU', dateOptions);
+  DATE.textContent = currentDate[0].toUpperCase() + currentDate.slice(1);
+}
+
+function showTime() {
+  const date = new Date();
+  const currentTime = date.toLocaleTimeString();
+  TIME.textContent = currentTime;
+  showDate();
+  setTimeout(showTime, 1000);
+}
+
+showTime();
