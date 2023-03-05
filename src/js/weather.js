7b403ffd6630cff870caca86d34042da75aeff67
EtoZheLamia <Andariel2175@yandex.ru>
2023-03-05 14:38:27 +0300

    init: start quote task

---
+++ src/js/weather.js
@@ -0,0 +1,6 @@
+
+
+
+// Для получения информации о погоде создаём ссылку https://api.openweathermap.org/data/2.5/weather?q=Москва&lang=ru&appid=3d33c09828dbe5b19caeb3d0b3bc829f&units=metric
+
+
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import './css/owfont-regular.css';
 import './js/time.js';
 import './js/image-slider.js';
 import './js/weather.js';
+import './js/quote.js';
 
 import {getLocalStorage, setLocalStorage} from './js/greeting';
 window.addEventListener('beforeunload', setLocalStorage);
