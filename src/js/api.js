5f342a4ab39a098206b0c693baa724d1577f5b15
EtoZheLamia <Andariel2175@yandex.ru>
2023-03-07 12:31:57 +0300

    feat:adding display and update quotes

--- src/js/api.js
+++ src/js/api.js
@@ -8,4 +8,13 @@ function getWeather(city, onSuccess, onFail) {
     });
 }
 
-export {getWeather};
+function getQuotes(onSuccess) {
+  const quotes = 'https://raw.githubusercontent.com/EtoZheLamia/momentum_assets/main/data.json';
+  fetch(quotes)
+    .then((response) => response.json())
+    .then((data) => {
+      onSuccess(data);
+    });
+}
+
+export {getWeather, getQuotes};
